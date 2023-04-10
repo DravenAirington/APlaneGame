feadea9c91e005e497386f99b2f39d185ff99a2a
DravenAirington <91614264+DravenAirington@users.noreply.github.com>
2023-04-10 14:43:13 -0500

    Lots and Lots of stufff

--- Assets/Scripts/Coin.cs
+++ Assets/Scripts/Coin.cs
@@ -5,28 +5,36 @@ using UnityEngine;
 public class Coin : MonoBehaviour
 {
 
-    private void OnTriggerEnter(Collider other)
+
+    private void Update()
     {
-        if(other.CompareTag("rusty coin"))
-        {
-            GameManager.instance.CollectCoin(1);
-            GameManager.instance.scoreTime += 3;
-        }
-        if (other.CompareTag("silver coin"))
-        {
-            GameManager.instance.CollectCoin(5);
-            GameManager.instance.scoreTime += 10;
-        }
-        if (other.CompareTag("Copper coin"))
-        {
-            GameManager.instance.CollectCoin(2);
-            GameManager.instance.scoreTime += 5;
-        }
-        if (other.CompareTag("Gold Coin"))
-        {
-            GameManager.instance.CollectCoin(10);
-            GameManager.instance.scoreTime += 15;
-        }
+        transform.Rotate((Vector3.forward * 75 + Vector3.right * 75 + Vector3.up * 75) *Time.deltaTime);
+
     }
+
+
+    /* private void OnTriggerEnter(Collider other)
+     {
+         if(other.CompareTag("rusty coin"))
+         {
+             GameManager.instance.CollectCoin(1);
+             GameManager.instance.scoreTime += 3;
+         }
+         if (other.CompareTag("silver coin"))
+         {
+             GameManager.instance.CollectCoin(5);
+             GameManager.instance.scoreTime += 10;
+         }
+         if (other.CompareTag("Copper coin"))
+         {
+             GameManager.instance.CollectCoin(2);
+             GameManager.instance.scoreTime += 5;
+         }
+         if (other.CompareTag("Gold Coin"))
+         {
+             GameManager.instance.CollectCoin(10);
+             GameManager.instance.scoreTime += 15;
+         }
+     }*/
 
 }
---
+++ Assets/Scripts/MobileHangarButtons.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MobileHangarButtons : MonoBehaviour
+{
+    public GameObject button1;
+    public GameObject button2;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+#if (UNITY_IOS && UNITY_ANDROID)
+
+#else
+        button1.SetActive(false);
+        button2.SetActive(false);
+#endif
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
